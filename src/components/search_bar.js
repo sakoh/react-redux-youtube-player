cc57c3a1986fa8266c187f4dacc5fb735bc2d855
sakoh <sakohartounian@yahoo.com>
2016-07-19 12:30:43 -0700

    basic input event set up for search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+
   render() {
     return (
-      <div className="search-bar-component">Search Bar</div>
+      <div className="search-bar-component form-group">
+        <input onChange={event => console.log(event.target.value)} type="text" class="form-control" placeholder="Search"/>
+      </div>
     );
   }
 }
