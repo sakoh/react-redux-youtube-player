5e204524027efbf8509ce532fcd4fd5eceeda298
sakoh <sakohartounian@yahoo.com>
2016-07-20 15:54:38 -0700

    middleware error is solved, but videos are not showing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { reduxThunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(reducers);
 
 render(
